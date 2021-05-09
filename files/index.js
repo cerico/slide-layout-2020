f75931cd477a99388ea1ebd3989f5bd131389275
picfair <garethrobertlee@gmail.com>
2021-05-09 06:53:52 +0100

    swap icon in light and dark modes

--- files/index.js
+++ files/index.js
@@ -13,3 +13,23 @@ document.getElementById('tab-three').onclick = function(){
   document.getElementById('tab-two').classList.add('in-view')
   document.getElementById('tab-three').classList.add('in-view')
 }
+
+const switchFavIcon = () => {
+  if (!window.matchMedia)
+  return;
+
+  const current = document.querySelectorAll('head > link[rel="icon"][media]')
+
+  current.forEach(function(icon) {
+    const match = window.matchMedia(icon.media);
+    function swap() {
+      if (match.matches) {
+        document.querySelector('head').appendChild(icon)
+      }
+    }
+    match.addEventListener('change', swap)
+    swap();
+  })
+}
+
+document.addEventListener("DOMContentLoaded", switchFavIcon)
